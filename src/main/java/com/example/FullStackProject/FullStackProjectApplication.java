ca2a1387ff4669db9939cac4fa988f0910752280
tsering651 <tseringwang4039@gmail.com>
2023-03-06 16:40:27 +0530

    Created a spring boot api for movies application

---
+++ src/main/java/com/example/FullStackProject/FullStackProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.FullStackProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FullStackProject/FullStackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FullStackProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
